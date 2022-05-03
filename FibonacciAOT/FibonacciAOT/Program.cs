02b04fac6a719a113b4b5e19610058e2e8566487
Gerardo Melendrez <payini@hotmail.com>
2022-05-03 02:26:54 -0700

    first commit

---
+++ FibonacciAOT/FibonacciAOT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+var numbersInSequence = 45;
+var executions = 10000;
+var stopWatch = new Stopwatch();
+var response = string.Empty;
+
+do
+{
+    stopWatch.Start();
+
+    for (int i = 1; i <= executions; i++)
+    {
+        Fibonacci(0, 1, 1, numbersInSequence);
+    }
+
+    stopWatch.Stop();
+
+    Console.WriteLine($"\n\nTotal Time elapsed for {executions} executions: {stopWatch.ElapsedMilliseconds} milliseconds.");
+    Console.WriteLine("\nHit enter to run away or N to exit.");
+
+    stopWatch.Reset();
+
+    response = Console.ReadLine();
+} while (response?.ToUpper() != "N");
+
+static void Fibonacci(int firstNumber, int secondNumber, int numbersProcessed, int numbersInSequence)
+{
+    Console.Write($"{firstNumber}{(numbersProcessed < numbersInSequence ? ", " : string.Empty)}");
+    if (numbersProcessed < numbersInSequence)
+    {
+        Fibonacci(secondNumber, firstNumber + secondNumber, numbersProcessed + 1, numbersInSequence);
+    }
+}
